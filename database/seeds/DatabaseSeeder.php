d448167743e735db4c70a9ef920178b553bff41d
phgrund <paulinhogrund@gmail.com>
2020-02-16 22:04:20 -0300

    :rocket: Seeding add

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'Paulo Henrique',
+            'email' => 'ph@web.com',
+            'password' => bcrypt('123'),
+            'cpf' => '123.456.789-10'
+        ]);
+
+        for($i = 0; $i < 10; $i++) {
+            DB::table('users')->insert([
+                'name' => Str::random(10),
+                'email' => Str::random(10).'@gmail.com',
+                'password' => bcrypt(Str::random(3)),
+                'cpf' => '123.456.789-10'
+            ]);
+        }
     }
 }
